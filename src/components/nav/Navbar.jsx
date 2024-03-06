78d7860c8a7894008448c225d643555fcec99f3b
KING <admin@panyor.com>
2024-03-06 18:36:55 +0000

    design update

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -5,13 +5,16 @@ function Navbar(){
     const { pathname } = useLocation();
 
     return (
-       <nav className="relative flex w-full flex-wrap items-center justify-between bg-red-300 py-2 text-neutral-700 dark:bg-slate-500 lg:py-4">
+       <nav className="relative flex w-full flex-wrap items-center justify-between bg-slate-900 py-2 text-neutral-700 dark:bg-slate-500 lg:py-4">
             <div className="flex w-full flex-wrap items-center justify-between px=3">
                 <div className="ml-2">
-                    <Link to={'/'} className="text-xl text-neutral-800 dark:text-neutral-200">Navbar</Link>
-                    <Link to={'/about'} className="text-xl text-neutral-800 dark:text-neutral-200">About</Link>
-                    <Link to={'/skills'} className="text-xl text-neutral-800 dark:text-neutral-200">Skills</Link>
-                    <Link to={'/projects'} className="text-xl text-neutral-800 dark:text-neutral-200">Projects</Link>
+                    <Link to={'/'} className="text-xl text-neutral-200 px-2">
+                        <div style={{backgroundImage: 'url(../assets/react.svg)'}}></div>
+                        <span>Matt Panyor</span>
+                    </Link>
+                    <Link to={'/about'} className="text-xl text-neutral-200 px-2">About</Link>
+                    <Link to={'/skills'} className="text-xl text-neutral-200 px-2">Skills</Link>
+                    <Link to={'/projects'} className="text-xl text-neutral-200 px-2">Projects</Link>
                 </div>
             </div>
        </nav>
